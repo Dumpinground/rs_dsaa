eae83f36d8f53501ad5da63b783d700508713f95
Dumpinground <e60533658@icloud.com>
2024-02-15 23:21:39 +0800

    update

--- examples/lesson_4.rs
+++ examples/lesson_4.rs
@@ -6,20 +6,19 @@ pub enum Res<T, E> {
     Error(E),
 }
 
-fn divide(a: i32, b: i32) -> Res<i32, String> {
+fn divide(a: i32, b: i32) -> Result<i32, String> {
     if b == 0 {
-        return Res::Error("Cannot divide by zero".to_string());
+        return Err("Cannot divide by zero".to_string());
     }
 
-    Res::Thing(a / b)
+    Ok(a / b)
 }
 
 fn main() {
     let (a, b) = (divide(10, 5), divide(10, 0));
 
-    match a {
-        Res::Thing(v) => println!("val = {}", v),
-        _ => {}
+    if let Ok(v) = a {
+        println!("val = {}", v);
     }
 
     println!("{:?} {:?}", a, b);
